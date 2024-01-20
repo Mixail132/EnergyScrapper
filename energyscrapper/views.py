bf8c9b12f876e0021bc6504d3241f567f882d978
Mixail132 <millionbaxov@gmail.com>
2024-01-20 15:21:58 +0300

    Add the database credentials
    Add dialect, driver, username, password, host, port, database file to create ae engine
    to connect to the database. Tried to connect - unresult.

--- energyscrapper/views.py
+++ energyscrapper/views.py
@@ -1,1 +1,11 @@
-print("Hello, world!")
+import sqlalchemy as db
+
+
+path='/C:/Users/Ev/Documents/Python/EnergyScrapper/ENERGY.GDB'
+db_uri='firebird+firebird://SYSDBA:masterkey@localhost:3050/ENERGY.GDB'
+engine=db.create_engine(db_uri, echo=True)
+
+connection = engine.connect()
+metadata = db.MetaData()
+impuls4 = db.Table(['impuls4'], metadata, autoload=True, autoload_with=engine)
+query = db.select([impuls4])
