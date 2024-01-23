ebf69f2e588b3561dea84336d945fa167c774102
Mixail132 <millionbaxov@gmail.com>
2024-01-23 15:44:21 +0300

    Add first ordered request to the database
    The request is trial. Just getting the data needed and see how it works.

--- energyscrapper/views.py
+++ energyscrapper/views.py
@@ -1,6 +1,6 @@
 import fdb
 
-con = fdb.connect(
+connect = fdb.connect(
     host='localhost',
     database='C:\Program Files (x86)\Control Center Server\Base\ENERGY.GDB',
     user='sysdba',
@@ -9,6 +9,9 @@ con = fdb.connect(
     fb_library_name='C://Program Files (x86)/Firebird/Firebird_2_1/bin/fbclient.dll'
 )
 
-cur = con.cursor()
-cur.execute("select * from impuls4")
-print(cur.fetchall())
+cursor = connect.cursor()
+request = "SELECT TARIF1, TARIF2, TARIF3 FROM IMPULS4 WHERE NUM_CH=1 AND NUM_SAM=1 ORDER BY DT_DAY"
+cursor.execute(request)
+
+for i in  cursor.fetchall():
+    print(i)
