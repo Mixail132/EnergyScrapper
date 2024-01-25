a5fcee137d78e6c2f3a42f153d0664e667c6f363
Mixail132 <millionbaxov@gmail.com>
2024-01-25 21:09:04 +0300

    Clear the file views.py
    The file will contain of the main logic in the future.
    Now all the functions from views.py have been moved to separate files.

--- energyscrapper/views.py
+++ energyscrapper/views.py
@@ -1,4 +1,5 @@
 import fdb
+from datetime import datetime
 
 connect = fdb.connect(
     host='localhost',
@@ -9,9 +10,38 @@ connect = fdb.connect(
     fb_library_name='C://Program Files (x86)/Firebird/Firebird_2_1/bin/fbclient.dll'
 )
 
-cursor = connect.cursor()
-request = "SELECT TARIF1, TARIF2, TARIF3 FROM IMPULS4 WHERE NUM_CH=1 AND NUM_SAM=1 ORDER BY DT_DAY"
-cursor.execute(request)
+cursor1 = connect.cursor()
+cursor2 = connect.cursor()
 
-for i in  cursor.fetchall():
-    print(i)
+request_1 = (
+"SELECT NAME, SERIALNUM, NUM_DEVICE, NUM_CH FROM CHANNEL "
+)
+request_2 = (
+"SELECT NUM_DEVICE, NUM_CH , DT_DAY,TARIF1, TARIF2, TARIF3 FROM IMPULS4 WHERE DT_DAY='2024-01-01' ORDER BY DT_DAY"
+)
+
+cursor1.execute(request_1)
+cursor2.execute(request_2)
+
+all_counters_info=[]
+for i in  cursor1.fetchall():
+    counters_info=[]
+    for j in i:
+        counters_info.append(str(j)) 
+    if not 'R-' in counters_info[0] :
+        if not 'R+' in counters_info[0]:
+            if not 'А-' in counters_info[0]:
+                if not 'A-' in counters_info[0]:
+                    all_counters_info.append(counters_info)
+for num, char  in enumerate (all_counters_info, 1):
+    print(num, char)
+    # all_counters_info.append(counters_info)
+        # counters_info.append((j[0],j[1],j[2],j[3]))
+    # day_energy_total=i[1]+i[2]+i[3]
+    # day_date = i[0].strftime("%Y-%m-%d")
+    # day_energy=round(day_energy_total,2)
+    # print(day_date, day_energy)
+
+# for j in cursor2.fetchall():
+#     print(j)
+# print(all_counters_info)
