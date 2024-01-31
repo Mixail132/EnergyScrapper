7dc440ee0c83a6181caf64547c8b5d0a883ddd00
Mixail132 <millionbaxov@gmail.com>
2024-01-31 22:57:29 +0300

    Add system paths to connections.py
    The paths to the database file and to the fbclient.dll now are
    variables placed in .env file cause they are different from
    computer to a computer.

--- energyscrapper/connections.py
+++ energyscrapper/connections.py
@@ -1,13 +1,20 @@
+import os
 import fdb
+from dotenv import load_dotenv
+
+
+load_dotenv()
+DATABASE = os.getenv("DATABASE_PATH")
+DB_CLIENT = os.getenv("DBCLIENT_PATH")
 
 
 connect = fdb.connect(
     host='localhost',
-    database='C:\Program Files (x86)\Control Center Server\Base\ENERGY.GDB',
+    database=DATABASE,
     user='sysdba',
     password='masterkey',
     charset="UTF-8",
-    fb_library_name='C://Program Files (x86)/Firebird/Firebird_2_1/bin/fbclient.dll'
+    fb_library_name=DB_CLIENT
 )
 
 cursor = connect.cursor()
