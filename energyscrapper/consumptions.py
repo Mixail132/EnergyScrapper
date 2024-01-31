06c123508f1cee3bc8a1368543103675155d9d4e
Mixail132 <millionbaxov@gmail.com>
2024-01-31 23:04:42 +0300

    Try fixing an TypeError in consumptions.py when execute
    If you try to execute consumptions.py there is an type error because of
    the same cursor object in different functions. It's still diesn't work.

--- energyscrapper/consumptions.py
+++ energyscrapper/consumptions.py
@@ -4,35 +4,35 @@ from counters import get_counters_info
 from connections import cursor
 
 
-yesterday = datetime.today()-timedelta(days=4)
+yesterday = datetime.today()-timedelta(days=16)
 sql_date = yesterday.strftime("%Y-%m-%d") 
-request = (f"SELECT NUM_DEVICE, NUM_CH , DT_DAY,TARIF1, TARIF2, TARIF3 FROM IMPULS4 WHERE DT_DAY='{sql_date}'")
-cursor.execute(request)
-counters = get_counters_info()
-consumptions = cursor.fetchall()
 
 
 def get_counters_consumption():
-    """ Gets energy consumption data from the database, 
-        looks for which the data belongs to which counter, 
-        makes a list of the summed up data. """
-    all_couters_consumption=[]
-    for counter in counters:
-        for consumption in consumptions:
-            one_counter_consumption=[]
+    """ Gets energy consumption data from the database,
+            looks for which the data belongs to which counter,
+            makes a list of the summed up data. """
+    counters = get_counters_info()
+    request = (f"SELECT NUM_DEVICE, NUM_CH , DT_DAY,TARIF1, TARIF2, TARIF3 FROM IMPULS4 WHERE DT_DAY='{sql_date}'")
+    cursor.execute(request)
+    all_counters_consumption = []
+    for consumption in cursor.fetchall():
+        for counter in counters:
+            one_counter_consumption = []
             if counter[3] == consumption[1]:
                 if counter[2] == consumption[0]:
                     one_counter_consumption.append(counter[1])
                     total_consumption = round(consumption[3]+consumption[4]+consumption[5], 2)
                     one_counter_consumption.append(total_consumption)
                     break
-        all_couters_consumption.append(one_counter_consumption)
-    return all_couters_consumption
+            all_counters_consumption.append(one_counter_consumption)
+    return all_counters_consumption
+
 
 
 if __name__ == "__main__":
-    consumptions = get_counters_consumption()
-    for num, char in enumerate(consumptions, 1):
+    all_consumptions = get_counters_consumption()
+    for num, char in enumerate(all_consumptions, 1):
         print(num, char)
 
 
