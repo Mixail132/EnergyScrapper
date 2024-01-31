b824f6851d217620a5be392f3ef3fd4fa07d18d3
Mixail132 <millionbaxov@gmail.com>
2024-01-31 23:02:14 +0300

    Replace sql statement and cursor object to the function
    The sql statement and the cursor object have been replaced to
    get_counters_info() function in counters.py cause there was an error
    when calling the function because of the same cursor object.

--- energyscrapper/counters.py
+++ energyscrapper/counters.py
@@ -3,27 +3,24 @@ import re
 from connections import cursor
 
 
-
-request = ("SELECT NAME, SERIALNUM, NUM_DEVICE, NUM_CH FROM CHANNEL")
-cursor.execute(request)
-
-
 def get_counters_info():
-    """ Gets counters' info (number of a counter, what it counts) 
-        from the database, filters the extra data and makes a list
-        of the data. """
-    all_counters_info=[]
-    for i in  cursor.fetchall():
-        counters_info=[]
+    """ Gets counters' info (number of a counter, what it counts)
+            from the database, filters the extra data and makes a list
+            of the data. """
+    request = ("SELECT NAME, SERIALNUM, NUM_DEVICE, NUM_CH FROM CHANNEL")
+    cursor.execute(request)
+    all_counters_info = []
+    for i in cursor.fetchall():
+        counters_info = []
         for j in i:
-            counters_info.append((j)) 
-        reg=r"R\-|R\+|A\-|А\-"
-        if not re.findall(reg,counters_info[0]):
+            counters_info.append((j))
+        reg = r"R\-|R\+|A\-|А\-"
+        if not re.findall(reg, str(counters_info[0])):
             all_counters_info.append(counters_info)
     return all_counters_info
 
 
 if __name__ == "__main__":
     counters = get_counters_info()
-    for num, char  in enumerate (counters, 1):
+    for num, char in enumerate(counters, 1):
         print(num, char)
