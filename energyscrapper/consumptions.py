f9ae538d34ee8289d78aabcb6effd4611f68c627
Mixail132 <millionbaxov@gmail.com>
2024-02-01 13:36:37 +0300

    Edit consumptions.py
    Add a parameter (data_date) to get_counters_consumption() function
    to make it possible to call it from different dates,
    replace the date determination to if name-main function,
    edit and make it more readable the SQL statement.

--- energyscrapper/consumptions.py
+++ energyscrapper/consumptions.py
@@ -4,34 +4,30 @@ from counters import get_counters_info
 from connections import cursor
 
 
-yesterday = datetime.today()-timedelta(days=16)
-sql_date = yesterday.strftime("%Y-%m-%d") 
 
-
-def get_counters_consumption():
-    """ Gets energy consumption data from the database,
-            looks for which the data belongs to which counter,
-            makes a list of the summed up data. """
-    counters = get_counters_info()
-    request = (f"SELECT NUM_DEVICE, NUM_CH , DT_DAY,TARIF1, TARIF2, TARIF3 FROM IMPULS4 WHERE DT_DAY='{sql_date}'")
+def get_counters_consumption(data_date):
+    """ Gets all the energy consumption data from the database. """
+    request = (f"""
+        SELECT 
+        NUM_DEVICE,
+        NUM_CH, 
+        DT_DAY,
+        TARIF1, 
+        TARIF2, 
+        TARIF3 
+        FROM IMPULS4 
+        WHERE 
+        DT_DAY='{data_date}'
+        """)
     cursor.execute(request)
-    all_counters_consumption = []
-    for consumption in cursor.fetchall():
-        for counter in counters:
-            one_counter_consumption = []
-            if counter[3] == consumption[1]:
-                if counter[2] == consumption[0]:
-                    one_counter_consumption.append(counter[1])
-                    total_consumption = round(consumption[3]+consumption[4]+consumption[5], 2)
-                    one_counter_consumption.append(total_consumption)
-                    break
-            all_counters_consumption.append(one_counter_consumption)
-    return all_counters_consumption
+    return cursor.fetchall()
 
 
 
 if __name__ == "__main__":
-    all_consumptions = get_counters_consumption()
+    yesterday = datetime.today()-timedelta(days=16)
+    sql_date = yesterday.strftime("%Y-%m-%d") 
+    all_consumptions = get_counters_consumption(sql_date)
     for num, char in enumerate(all_consumptions, 1):
         print(num, char)
 
