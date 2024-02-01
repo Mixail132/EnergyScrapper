5d3a6fd8c3cc0b0f78c12be16ac9879478fb0586
Mixail132 <millionbaxov@gmail.com>
2024-02-01 17:49:56 +0300

    Add a main logic to views.py
    For now the main logic is:
    Getting all the counters info from the database
    Getting the consumptions for the counters for a certain date
    Filtering the extra consumption data
    Summorize and round the consumption values
    Combine the counter's info and the related consumption value
    to a separate list.

--- energyscrapper/views.py
+++ energyscrapper/views.py
@@ -1,4 +1,35 @@
 # Here willbe the main logic
+from datetime import datetime, timedelta
+from consumptions import get_counters_consumption
+from counters import get_counters_info
+
+
+def filter_retreived_energy():
+    yesterday = datetime.today()-timedelta(days=16)
+    sql_date  = yesterday.strftime("%Y-%m-%d")
+    consumptions = get_counters_consumption(sql_date)
+    counters = get_counters_info()
+    filtred_consumption = [] 
+    for counter in counters:
+        for consumption in consumptions:
+            if counter[3] == consumption[1]:
+                if counter[2] == consumption[0]:
+                    one_counter_consumption=[]
+                    one_counter_total_energy = round(sum(consumption[3:6], 2))
+                    one_counter_consumption.append(counter[1])
+                    one_counter_consumption.append(one_counter_total_energy)
+                    break
+        filtred_consumption.append(one_counter_consumption)
+    return filtred_consumption
+
+
+if __name__=="__main__":
+    filtered_energy = filter_retreived_energy() 
+    for num, char in enumerate (filtered_energy, 1):
+        print(num, char)
+
+
+
 
 # def get_counters_consumption():
 #     """ Gets energy consumption data from the database,
