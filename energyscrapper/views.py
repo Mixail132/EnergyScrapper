7992e5fb857e2bae791913b10f01a2ad41db84d6
Mixail132 <millionbaxov@gmail.com>
2024-02-02 16:29:19 +0300

    Change data type of filtered_consumption in views.py
    The list of lists now became a dictionary cause it's lot easier to operate
    it's items. The one counter consumption list is no longer needed.

--- energyscrapper/views.py
+++ energyscrapper/views.py
@@ -1,4 +1,4 @@
-# Here willbe the main logic
+# Here will be the main logic
 from datetime import datetime, timedelta
 from consumptions import get_counters_consumption
 from counters import get_counters_info
@@ -12,20 +12,17 @@ def filter_retreived_energy():
     sql_date  = yesterday.strftime("%Y-%m-%d")
     consumptions = get_counters_consumption(sql_date)
     counters = get_counters_info()
-    filtred_consumption = [] 
+    filtered_consumption={} 
     for counter in counters:
         for consumption in consumptions:
             if counter[3] == consumption[1] and counter[2] == consumption[0]:
-                one_counter_consumption=[]
-                one_counter_total_energy = round(sum(consumption[3:6], 2))
-                one_counter_consumption.append(counter[1])
-                one_counter_consumption.append(one_counter_total_energy)
+                one_counter_total_energy = (sum(consumption[3:6]))
+                filtered_consumption[counter[1]] = round(one_counter_total_energy, 2)
                 break
-        filtred_consumption.append(one_counter_consumption)
-    return filtred_consumption
+    return filtered_consumption
 
 
 if __name__=="__main__":
     filtered_energy = filter_retreived_energy() 
-    for num, char in enumerate (filtered_energy, 1):
-        print(num, char)
+    for key, value in filtered_energy.items():
+        print(key, value)
