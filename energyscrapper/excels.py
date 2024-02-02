6fe7f2090bef0ef7dfa5cbec7e83047b406cb1a2
Mixail132 <millionbaxov@gmail.com>
2024-02-02 17:05:15 +0300

    Delete circular import from excels.py
    The circular import statement refers to views.py.

--- energyscrapper/excels.py
+++ energyscrapper/excels.py
@@ -3,22 +3,21 @@ from openpyxl import load_workbook
 import os
 import datetime
 from dotenv import load_dotenv
-from views import filter_retreived_energy
+# from views import filter_retreived_energy
 
 
 load_dotenv()
-counters_storage_file = os.getenv('DBEXCEL_PATH')
-counters_data_storage =  load_workbook(counters_storage_file) 
-today_date = datetime.datetime.now()
 
 
 def put_consumptions_to_excel(counters_data):
+    counters_storage_file = os.getenv('DBEXCEL_PATH')
+    counters_data_storage =  load_workbook(counters_storage_file) 
+    today_date = datetime.datetime.now()
     current_date_row = today_date.toordinal()-737328
     counters_numbers={}
     for col_number in range(2, 47):
         counter_number = counters_data_storage.active.cell(row=3, column = col_number).value
         counters_numbers[counter_number] = col_number
-    # print(counters_numbers[18085116])
     for counter_number, col_number in counters_numbers.items():
         try:
             energy_value = (counters_data[str(counter_number)])
