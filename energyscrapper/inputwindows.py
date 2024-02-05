e4780c03427c98644052dad845dabd4230fb0ea5
Mixail132 <millionbaxov@gmail.com>
2024-02-05 14:10:58 +0300

    Add a class to inputwindows.py as a user input object handler
    The class 'UserInput' is added and there are two methods in it:
    first - for making a window for user input
    second - for gatting the data from user input window.

--- energyscrapper/inputwindows.py
+++ energyscrapper/inputwindows.py
@@ -3,12 +3,32 @@
 import tkinter as tk
 from tkinter import messagebox
 
-window = tk.Tk()
-window.title("EnergyControlCenter")
-days_label = tk.Label(window, text="Введите количество дней для опроса данных ")
-days_label.pack()
-days_entry = tk.Entry(window)
-days_entry.pack()
-get_button = tk.Button(text="Опросить", command="")
-get_button.pack()
-window.mainloop()
+
+class UserInput:
+    """ Handles the user's input """
+
+    def get_days_from_user(self):        
+        d =  self.days_entry.get()
+        print(d , type(d))
+
+
+    def make_user_input(self):
+        """ 
+        Creates a user input window and takes text from a user.
+        The text 
+        """
+        window = tk.Tk()
+        window.title("EnergyControlCenter")
+        days_label = tk.Label(window, text="Введите количество дней для опроса данных ")
+        days_label.pack()
+        self.days_entry = tk.Entry(window)
+        self.days_entry.pack()
+        get_button = tk.Button(text="Опросить", command=self.get_days_from_user)
+        get_button.pack()
+        # window.after(5000, lambda: window.destroy())
+        window.mainloop()
+
+
+if __name__=="__main__":
+    win=UserInput()
+    win.make_user_input()
