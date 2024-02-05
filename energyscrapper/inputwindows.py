39d93c6a1ab3a086f35168a754286815a2ec9b97
Mixail132 <millionbaxov@gmail.com>
2024-02-05 17:40:20 +0300

    Modify the function get_days_from_user() in inputwindows.py
    Add a comment to the function, add a global variable for get an access from
    other functionm, set destroy() method for closing the user input window
    once the user inputs a number.

--- energyscrapper/inputwindows.py
+++ energyscrapper/inputwindows.py
@@ -7,28 +7,37 @@ from tkinter import messagebox
 class UserInput:
     """ Handles the user's input """
 
-    def get_days_from_user(self):        
-        d =  self.days_entry.get()
-        print(d , type(d))
+
+    def get_days_from_user(self):
+        """ 
+        Gets the text from user input window
+        and make it as an int.
+        """        
+        global days 
+        days = self.days_entry.get()
+        self.window.destroy()
 
 
     def make_user_input(self):
         """ 
         Creates a user input window and takes text from a user.
-        The text 
+        The text is number of days for getting data from the DB.
         """
-        window = tk.Tk()
-        window.title("EnergyControlCenter")
-        days_label = tk.Label(window, text="Введите количество дней для опроса данных ")
-        days_label.pack()
-        self.days_entry = tk.Entry(window)
+        self.window = tk.Tk()
+        self.window.title("EnergyControlCenter")
+        self.days_label = tk.Label(self.window, text="Введите количество дней для опроса данных ")
+        self.days_label.pack()
+        self.days_entry = tk.Entry(self.window)
         self.days_entry.pack()
-        get_button = tk.Button(text="Опросить", command=self.get_days_from_user)
-        get_button.pack()
-        # window.after(5000, lambda: window.destroy())
-        window.mainloop()
+        self.days_entry.insert(0, 3)
+        self.get_button = tk.Button(text="Опросить", command=self.get_days_from_user)
+        self.get_button.pack()
+        self.window.mainloop()
+        return int(days)
+     
 
 
 if __name__=="__main__":
-    win=UserInput()
-    win.make_user_input()
+    win = UserInput()
+    print(win.make_user_input())
+    # print(win.get_days_from_user())
