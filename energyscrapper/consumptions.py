e2b1d249e2c4ff33596f0118884d42cb5f346b97
Mixail132 <millionbaxov@gmail.com>
2024-02-16 21:24:31 +0300

    Replace filtering function to consumptions.py
    Now excluding reactive and revert energy has been replaced to here. This might reduce the code

--- energyscrapper/consumptions.py
+++ energyscrapper/consumptions.py
@@ -1,6 +1,7 @@
 # The script gets the energy consumption info
 from datetime import datetime, timedelta
 from connections import cursor
+from counters import get_counters_info
 
 
 def get_counters_consumption(data_date: str) -> cursor:
@@ -29,11 +30,26 @@ def get_counters_consumption(data_date: str) -> cursor:
     return cursor.fetchall()
 
 
+def filter_counters_consumption(counters, consumptions):
+    one_day_filtered_consumption = {}
+    for counter in counters:
+        for consumption in consumptions:
+            if counter[3] == consumption[1] and counter[2] == consumption[0]:
+                one_counter_total_energy = (sum(consumption[3:6]))
+                one_day_filtered_consumption[counter[1]] = round(one_counter_total_energy, 2)
+                break
+    return one_day_filtered_consumption
+
+
 if __name__ == "__main__":
     example_date = datetime.today()-timedelta(days=3)
     sql_date = example_date.strftime("%Y-%m-%d")
     all_consumptions = get_counters_consumption(sql_date)
-    for num, char in enumerate(all_consumptions, 1):
-        print(num, char)
+    counters_info = get_counters_info()
+    filtered = filter_counters_consumption(counters_info, all_consumptions)
+    for i, key in enumerate(filtered.items()):
+        print(i, key)
+    # for num, char in enumerate(all_consumptions, 1):
+    #     print(num, char)
 
 
