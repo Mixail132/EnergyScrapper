d71c78bb9d46bfea4e8065ff91fad2d1cb976809
Mixail132 <millionbaxov@gmail.com>
2024-02-16 17:41:12 +0300

    Try to get a period data from the DB
    This code has not worked yet.

--- energyscrapper/periodviews.py
+++ energyscrapper/periodviews.py
@@ -19,15 +19,16 @@ def day_energy_handler(sql_date: str, data_day: datetime) -> dict:
     """
     consumptions = get_counters_consumption(sql_date)
     counters = get_counters_info()
-    filtered_consumption={} 
+    all_filtered_consumption = {}
+    one_filtered_consumption = {}
     for counter in counters:
         for consumption in consumptions:
             if counter[3] == consumption[1] and counter[2] == consumption[0]:
                 one_counter_total_energy = (sum(consumption[3:6]))
-                filtered_consumption[counter[1]] = round(one_counter_total_energy, 2)
+                one_filtered_consumption[counter[1]] = round(one_counter_total_energy, 2)
                 break
-    put_consumptions_to_excel(data_day, filtered_consumption)
-    return filtered_consumption
+    # put_consumptions_to_excel(data_day, filtered_consumption)
+    return one_filtered_consumption
 
 
 def period_energy_handler() -> None:
@@ -41,13 +42,13 @@ def period_energy_handler() -> None:
         update_days = user_input.make_user_input() - 1
     except TypeError:
         return
-    for day in range(update_days, -1, -1):
-        data_day = datetime.today()-timedelta(days=day)
-        sql_date = data_day.strftime("%Y-%m-%d")
-        day_energy_handler(sql_date, data_day)
+    # for day in range(update_days, -1, -1):
+    data_day = datetime.today()-timedelta(days=update_days )
+    sql_date = data_day.strftime("%Y-%m-%d")
+    day_energy_handler(sql_date, data_day)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     print(time.strftime("%X"))
     period_energy_handler()
     print(time.strftime("%X"))
