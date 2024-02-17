a4ab38482ecbc73aa0f34b71d54fd1d192c07c23
Mixail132 <millionbaxov@gmail.com>
2024-02-17 12:57:04 +0300

    Fix 4 weak warnings in 'excels.py'

--- energyscrapper/excels.py
+++ energyscrapper/excels.py
@@ -2,10 +2,6 @@
 from openpyxl import load_workbook
 import os
 from dotenv import load_dotenv
-<<<<<<< HEAD
-from periodviews import period_energy_handler
-=======
->>>>>>> c2549eed45b38fff60e074fe0f2421f24525bed1
 
 
 load_dotenv()
@@ -13,11 +9,11 @@ load_dotenv()
 
 def put_consumptions_to_excel(data_day, counters_data):
     counters_storage_file = os.getenv('DBEXCEL_PATH')
-    counters_data_storage =  load_workbook(counters_storage_file) 
+    counters_data_storage = load_workbook(counters_storage_file)
     current_date_row = data_day.toordinal()-737328
-    counters_numbers={}
+    counters_numbers = {}
     for col_number in range(2, 47):
-        counter_number = counters_data_storage.active.cell(row=3, column = col_number).value
+        counter_number = counters_data_storage.active.cell(row=3, column=col_number).value
         counters_numbers[counter_number] = col_number
     for counter_number, col_number in counters_numbers.items():
         try:
@@ -25,14 +21,5 @@ def put_consumptions_to_excel(data_day, counters_data):
         except KeyError:
             continue
         column_number = counters_numbers[counter_number]  
-        counters_data_storage.active.cell(row=current_date_row, column=column_number).value=energy_value       
+        counters_data_storage.active.cell(row=current_date_row, column=column_number).value = energy_value
     counters_data_storage.save(counters_storage_file)
-
-
-<<<<<<< HEAD
-if __name__ == "__main__" :
-    counters_data = period_energy_handler()
-    put_consumptions_to_excel(counters_data)
-
-=======
->>>>>>> c2549eed45b38fff60e074fe0f2421f24525bed1
