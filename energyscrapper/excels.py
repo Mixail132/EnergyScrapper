48753b5c7fe6ed53a7e5af50fc6a6e52469c5cf6
Mixail132 <millionbaxov@gmail.com>
2024-02-24 21:27:35 +0300

    Save the data to Excel file line by line
    Modify the function in excels.py to write all the given data to Excel
    file at one time.

--- energyscrapper/excels.py
+++ energyscrapper/excels.py
@@ -1,6 +1,6 @@
 # Data storage in excel handler
 from openpyxl import load_workbook
-import os
+from datetime import datetime
 from dotenv import load_dotenv
 import dirs
 
@@ -11,16 +11,18 @@ load_dotenv()
 def put_consumptions_to_excel(data_day, counters_data):
     counters_storage_file = dirs.DB_EXCEL
     counters_data_storage = load_workbook(counters_storage_file)
-    current_date_row = data_day.toordinal()-737328
-    counters_numbers = {}
-    for col_number in range(2, 47):
-        counter_number = counters_data_storage.active.cell(row=3, column=col_number).value
-        counters_numbers[counter_number] = col_number
-    for counter_number, col_number in counters_numbers.items():
-        try:
-            energy_value = (counters_data[str(counter_number)])
-        except KeyError:
-            continue
-        column_number = counters_numbers[counter_number]  
-        counters_data_storage.active.cell(row=current_date_row, column=column_number).value = energy_value
+    for date, counters_values in counters_data.items():
+        date_to_paste = datetime.strptime(date, "%Y-%m-%d")
+        current_date_row = date_to_paste.toordinal()-737328
+        counters_numbers = {}
+        for col_number in range(2, 47):
+            counter_number = counters_data_storage.active.cell(row=3, column=col_number).value
+            counters_numbers[counter_number] = col_number
+        for counter_number, col_number in counters_numbers.items():
+            try:
+                energy_value = (counters_values[str(counter_number)])
+            except KeyError:
+                continue
+            column_number = counters_numbers[counter_number]
+            counters_data_storage.active.cell(row=current_date_row, column=column_number).value = energy_value
     counters_data_storage.save(counters_storage_file)
