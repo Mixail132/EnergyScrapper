77c021679399d1ca328a353da9ddfe65e3564ee8
Mixail132 <millionbaxov@gmail.com>
2024-04-03 21:10:07 +0300

    Organize the script and fix PEP8 comments.

--- excelupdater/updater.py
+++ excelupdater/updater.py
@@ -1,28 +1,42 @@
 import psutil
 import subprocess
 import os
-from pywinauto.application import Application
-import pyautogui as pg
-from dotenv import load_dotenv
 import time
+from pyautogui import hotkey
+from dotenv import load_dotenv
+from pywinauto.application import Application
+
 
 load_dotenv()
 main_excel_file = os.getenv("TGEXCEL_PATH")
 main_excel_title = os.getenv("TGEXCEL_NAME")
 
-names = []
-for process in psutil.process_iter():
-    name = process.name()
-    if name and name in "EXCELExcelexcel":
-        break
-else:
-    subprocess.Popen(["start", main_excel_file], shell=True)
+
+def check_excel_running():
+    """ Scan the system processes and launch Excel if it's not running."""
+    for process in psutil.process_iter():
+        name = process.name()
+        if name and name in "EXCELExcelexcel":
+            break
+    else:
+        subprocess.Popen(["start", main_excel_file], shell=True)
+
+
+def set_excel_focus():
+    """ Finds a particular Excel file and sets focus on the file's window. """
+    app = Application().connect(title_re="energy.xlsx")
+    app[main_excel_title].set_focus()
+
+
+def run_excel_macro(key: str):
+    """ Presses the preset hotkey combination to run Excel macro. """
+    hotkey("ctrl", key)
+
+
+if __name__ == "__main__":
+    check_excel_running()
     time.sleep(5)
-
-
-app = Application().connect(title_re="energy.xlsx")
-
-app[main_excel_title].set_focus()
-
-# pg.hotkey("ctrl","m")
-
+    set_excel_focus()
+    run_excel_macro("m")
+    time.sleep(5)
+    run_excel_macro("ь")
