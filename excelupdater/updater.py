e1ee21b246fee21dd62aa99bbbdf393f0a093540
Mixail132 <millionbaxov@gmail.com>
2024-04-25 18:33:32 +0300

    Add paths to Excel files.

--- excelupdater/updater.py
+++ excelupdater/updater.py
@@ -6,10 +6,15 @@ from pyautogui import hotkey
 from dotenv import load_dotenv
 from pywinauto.application import Application
 
+# DATABASE_PATH = "C:\\Program Files (x86)\\Control Center Server\\Base\\ENERGY.GDB"
+# DBCLIENT_PATH = "C:\\Program Files (x86)\\Firebird\\Firebird_2_1\\bin\\fbclient.dll"
+# DBEXCEL_PATH = "C:\\Users\\User\\Documents\\Информация\\Расчеты\\Энергопотребление\\Показания\\Разное\\energy.xlsx"
+TGEXCEL_PATH="C:\\Users\\User\\Documents\\Информация\\Расчеты\\Энергопотребление\\Показания\\Разное\\Показания_ФСК.xlsx"
+TGEXCEL_NAME="Показания_ФСК.xlsx [Общий] - Excel"
 
 load_dotenv()
-main_excel_file = os.getenv("TGEXCEL_PATH")
-main_excel_title = os.getenv("TGEXCEL_NAME")
+main_excel_file = TGEXCEL_PATH
+main_excel_title = TGEXCEL_NAME
 
 
 def check_excel_running():
@@ -17,14 +22,16 @@ def check_excel_running():
     for process in psutil.process_iter():
         name = process.name()
         if name and name in "EXCELExcelexcel":
+            print("Excel is running")
             break
     else:
         subprocess.Popen(["start", main_excel_file], shell=True)
+        
 
 
 def set_excel_focus():
     """ Finds a particular Excel file and sets focus on the file's window. """
-    app = Application().connect(title_re="energy.xlsx")
+    app = Application().connect(title_re="Показания_ФСК.xlsx")
     app[main_excel_title].set_focus()
 
 
